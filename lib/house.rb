a2e7515b9bb82e7d746cf449d2236514b4d271dc
Katelyn <katelyn.rimkunas@gmail.com>
2018-08-29 14:13:13 -0600

    Create additional house functionality (iteration 4)

--- lib/house.rb
+++ lib/house.rb
@@ -27,6 +27,24 @@ class House
     @rooms.map { |room| room.area }.sum
   end
 
-  
+  def price_per_square_foot
+    size = self.area
+    value = @price.dup
+    value.delete!("$")
+    price_per_sqft = value.to_f / size
+    price_per_sqft.round(2)
+  end
+
+  def rooms_sorted_by_area
+    @rooms.sort_by { |room| room.area }
+  end
+
+  def rooms_by_category
+    # {room.symbol [] }
+    hash = {}
+    syms = @rooms.map {|room| room.category }
+    syms.uniq.each {|sym| hash[sym] = rooms_from_category(sym) }
+    return hash
+  end
 
 end
